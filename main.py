feaabeb86552823d3c5a3951c6c8d4572f211412
jbachaalany <111648069+jbachaalany@users.noreply.github.com>
2022-12-21 20:14:42 +0000

    updated the while loop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from art import logo
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(start_text, shift_amount, cipher_direction):
@@ -13,16 +12,17 @@ def caesar(start_text, shift_amount, cipher_direction):
       new_text += letter
   print(f"The {cipher_direction}d text is: {new_text}")
 
-
 print(logo)
- 
-choice = 'yes'
-while choice == 'yes':
+should_continue = True
+while should_continue:
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
   text = input("Type your message:\n").lower()
   shift = int(input("Type the shift number:\n"))
   
   caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
-  choice = input("Do you wish to continue? enter 'yes' or 'no'\n").lower()
+  result = input("Do you wish to continue? enter 'yes' or 'no'\n").lower()
+  if result != 'yes':
+    print("Goodbye")
+    should_continue = False
   print('------------------------------------------------')
